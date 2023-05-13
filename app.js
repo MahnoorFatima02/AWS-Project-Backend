355b15a4610cf74f98785f87428b4e36cdab414f
Sandesh Hyoju <sundesz@gmail.com>
2023-05-13 22:14:52 +0300

    Add port to env and change it to 8080

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const app = express()
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 const auth = require('./middleware/auth')
 const db = require('./database/db')
-const port = 3000
+const port = process.env.PORT || 8080
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.send('<h1>AWS project</h1>')
+})
+
 app.post('/login', (req, res) => {
-  auth.login(req, res);
-});
+  auth.login(req, res)
+})
 
-app.get('/employee/:id/salaries', auth.authenticateJWT, auth.salaryAccess, (req, res) => {
-	db.getEmployeeSalaries(req, res);
-});
+app.get(
+  '/employee/:id/salaries',
+  auth.authenticateJWT,
+  auth.salaryAccess,
+  (req, res) => {
+    db.getEmployeeSalaries(req, res)
+  }
+)
 
-app.get('/employee/:id', auth.authenticateJWT, auth.ownInformationAccess, (req, res) => {
-	db.getEmployee(req, res);
-});
+app.get(
+  '/employee/:id',
+  auth.authenticateJWT,
+  auth.ownInformationAccess,
+  (req, res) => {
+    db.getEmployee(req, res)
+  }
+)
 
 app.use(auth.requestLogger)
 // Middle ware and other uses for the app
